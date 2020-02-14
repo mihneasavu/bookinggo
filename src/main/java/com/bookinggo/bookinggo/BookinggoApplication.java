6aadeb0f22beda92143219034ac878c741814e26
mihneasavu <mihnea.savu@gmail.com>
2020-02-14 10:12:26 +0000

    First commit

---
+++ src/main/java/com/bookinggo/bookinggo/BookinggoApplication.java
@@ -0,0 +1,13 @@
+package com.bookinggo.bookinggo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookinggoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookinggoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookinggo/bookinggo/BookinggoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookinggo.bookinggo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookinggoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
